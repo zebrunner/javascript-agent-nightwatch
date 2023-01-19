23ee2d9b826fc4e564c2d21b795969116187f9cb
alexkirillovtech <alexandr.kirillov@ext.myfitnesspal.com>
2023-01-19 20:20:39 +0100

    init application

---
+++ .eslintrc.js
@@ -0,0 +1,29 @@
+module.exports = {
+    env: {
+        browser: true,
+        es6: true,
+        jest: true,
+        node: true,
+    },
+    extends: ['eslint:recommended', 'airbnb-base'],
+    plugins: ['prettier'],
+    globals: {
+        Atomics: 'readonly',
+        SharedArrayBuffer: 'readonly',
+    },
+    parserOptions: {
+        ecmaVersion: 2021,
+        sourceType: 'module',
+    },
+    rules: {
+        'no-console': 0,
+        indent: ['error', 2, {ignoreComments: true, SwitchCase: 1}],
+        'max-len': 0,
+        'consistent-return': 0,
+        camelcase: 0,
+        'no-underscore-dangle': 0,
+        'no-promise-executor-return': 0,
+        'no-template-curly-in-string': 0,
+        'new-cap': 0,
+    },
+};
