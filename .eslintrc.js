6405e8dcb5db6ce98daf9643c8c8f8cc07c0e219
alexkirillovtech <alexandr.kirillov@ext.myfitnesspal.com>
2023-02-01 12:30:35 +0100

    register test run start

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,29 +1,34 @@
 module.exports = {
-    env: {
-        browser: true,
-        es6: true,
-        jest: true,
-        node: true,
-    },
-    extends: ['eslint:recommended', 'airbnb-base'],
-    plugins: ['prettier'],
-    globals: {
-        Atomics: 'readonly',
-        SharedArrayBuffer: 'readonly',
-    },
-    parserOptions: {
-        ecmaVersion: 2021,
-        sourceType: 'module',
-    },
-    rules: {
-        'no-console': 0,
-        indent: ['error', 2, {ignoreComments: true, SwitchCase: 1}],
-        'max-len': 0,
-        'consistent-return': 0,
-        camelcase: 0,
-        'no-underscore-dangle': 0,
-        'no-promise-executor-return': 0,
-        'no-template-curly-in-string': 0,
-        'new-cap': 0,
-    },
+  env: {
+    browser: true,
+    es6: true,
+    jest: true,
+    node: true,
+  },
+  extends: ['eslint:recommended', 'airbnb-base'],
+  globals: {
+    Atomics: 'readonly',
+    SharedArrayBuffer: 'readonly',
+  },
+  parserOptions: {
+    ecmaVersion: 2021,
+    sourceType: 'module',
+  },
+  rules: {
+    'no-console': 0,
+    indent: ['error', 2, { ignoreComments: true, SwitchCase: 1 }],
+    'max-len': 0,
+    'consistent-return': 0,
+    camelcase: 0,
+    'no-underscore-dangle': 0,
+    'no-promise-executor-return': 0,
+    'no-template-curly-in-string': 0,
+    'new-cap': 0,
+    'padding-line-between-statements': [
+      'error',
+      { blankLine: 'always', prev: '*', next: 'return' },
+      { blankLine: 'always', prev: '*', next: 'if' },
+      { blankLine: 'always', prev: '*', next: 'break' },
+    ],
+  },
 };
